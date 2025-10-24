f887c257599c825fa5d6e7d07d25fba5c90e1120
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-10-24 19:59:34 +0200

    Update comment for mip odds URL

--- mip-odds.R
+++ mip-odds.R
@@ -7,7 +7,7 @@ today <- Sys.Date()
 # ensure data output folder exists
 if (!dir.exists("data")) dir.create("data")
 
-# url for getting mvp odds from rotowire
+# url for getting mip odds from rotowire
 url <- "https://www.rotowire.com/betting/nba/tables/player-futures.php?future=Most%20Improved%20Player"
 
 # get data
